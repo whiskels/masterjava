0235207f8f3b981bbc4631ae201b2530537f4c43
whiskels <kuzmingeo@gmail.com>
2020-11-19 13:00:43 +0300

    refactoring: 3 4 StAX

--- src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -15,6 +15,27 @@ public class StaxStreamProcessor implements AutoCloseable {
         reader = FACTORY.createXMLStreamReader(is);
     }
 
+    public boolean startElement(String element, String parent) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            if (parent != null && isElementEnd(event, parent)) {
+                return false;
+            }
+            if (isElementStart(event, element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isElementStart(int event, String el) {
+        return event == XMLEvent.START_ELEMENT && el.equals(reader.getLocalName());
+    }
+
+    private boolean isElementEnd(int event, String el) {
+        return event == XMLEvent.END_ELEMENT && el.equals(reader.getLocalName());
+    }
+
     public XMLStreamReader getReader() {
         return reader;
     }
